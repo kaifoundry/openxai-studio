77fcb10fbf2b446469883d5bbbb951b0df50279d
QuadbCompany <neeraja.naru@quadbtech.com>
2025-08-26 12:23:21 +0530

    feat: increased animation speed and animation consistancy

--- components/SidebarNav/sibebar-nav.tsx
+++ components/SidebarNav/sibebar-nav.tsx
@@ -753,7 +753,7 @@ const NavContainer = React.forwardRef<
     
       <aside
         className={cn(
-          'duration-plico sticky top-20 flex h-[calc(100svh-5rem)] shrink-0 flex-col px-0 justify-between  bg-[#1C1E2A] text-card-foreground transition-[width] delay-300 duration-500 ease-in-out max-hdplus:top-16',
+          'duration-plico sticky top-20 flex h-[calc(100svh-8.5rem)] shrink-0 flex-col px-0 justify-between  bg-[#1C1E2A] text-card-foreground transition-[width] delay-300 duration-500 ease-in-out max-hdplus:top-16',
           collapsed ? 'w-14' : 'w-64 max-hdplus:w-52',
           demoMode && 'top-24 max-hdplus:top-20',
           className
