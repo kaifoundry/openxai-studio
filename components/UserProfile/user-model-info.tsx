4d19d4171715d60701978cda19d55d672d323bf5
priyakumariyt05 <priya.kumari@quadbtech.com>
2025-09-11 12:42:46 +0530

    feat: created frontend folder structure for user profile page

---
+++ components/UserProfile/user-model-info.tsx
@@ -0,0 +1,10 @@
+import type { User } from 'lucide-react'
+import React from 'react'
+
+const UserModelInfo = () => {
+  return (
+    <div>user-model-info</div>
+  )
+}
+
+export default UserModelInfo
---
+++ components/UserProfile/user-profile-details.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const UserProfileDetails = () => {
+  return (
+    <div>user-profile-details</div>
+  )
+}
+
+export default UserProfileDetails
