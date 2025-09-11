7bc62a83660527b8b2d9dcc320a6f96ab70e062e
QuadbCompany <neeraja.naru@quadbtech.com>
2025-09-11 17:59:12 +0530

    feat: Added user deployed and undeployed app component in user profile

--- app/userProfile/[id]/page.tsx
+++ app/userProfile/[id]/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ModelDefinitions from '@/utils/model-definitions.json'
 import UserProfileDetails from '@/components/UserProfile/user-profile-details'
-
+import UserModelInfo from '@/components/UserProfile/user-model-info'
 interface PageProps {
     params: { id: string }
 }
@@ -13,6 +13,7 @@ const Page = ({ params }: PageProps) => {
     return (
         <div className="mx-auto w-full px-4 py-6">
             <UserProfileDetails name={name} image={image} />
+            <UserModelInfo/>
         </div>
     )
 }
