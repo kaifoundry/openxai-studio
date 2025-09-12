6861baf0f67e1a3328b62ff9e67eb1dbdf2e45de
QuadbCompany <neeraja.naru@quadbtech.com>
2025-09-12 11:57:01 +0530

    feat: Added filter and search functionality in user profile deployed app section with responsive for large devices

--- components/UserProfile/DeployedApps.tsx
+++ components/UserProfile/DeployedApps.tsx
@@ -1,32 +1,44 @@
-"use client";
-import React, { useState } from "react";
-import Filter from "./filter";
-import Deployed_models from "./Deployed_models";
+'use client'
+
+import React, { useState } from 'react'
+
+import Deployed_models from './Deployed_models'
+import Filter from './filter'
 
 const DeployedApps = () => {
-  const [show, setShow] = useState(true);
+  const [show, setShow] = useState(true)
+  const [selectedChains, setSelectedChains] = useState<string[]>([])
+  const [selectedCategories, setSelectedCategories] = useState<string[]>([])
 
   return (
-    <div className="flex w-full h-full">
-      {/* Sidebar */}
+    <div className="flex h-full w-full">
       <div
         className={`transition-all duration-700 ease-in-out ${
-          show ? "w-[35%] opacity-100" : "w-0 opacity-0"
+          show ? 'w-[35%] opacity-100' : 'w-0 opacity-0'
         } overflow-hidden`}
       >
-        <Filter />
+        <Filter
+          selectedChains={selectedChains}
+          setSelectedChains={setSelectedChains}
+          selectedCategories={selectedCategories}
+          setSelectedCategories={setSelectedCategories}
+        />
       </div>
 
-      {/* Main Content */}
       <div
-        className={`transition-all duration-700 ease-in-out ${
-          show ? "w-[65%]" : "w-full"
+        className={`pr-6 transition-all duration-700 ease-in-out ${
+          show ? 'w-[65%]' : 'w-full'
         }`}
       >
-        <Deployed_models setShow={setShow} show={show} />
+        <Deployed_models
+          setShow={setShow}
+          show={show}
+          selectedChains={selectedChains}
+          selectedCategories={selectedCategories}
+        />
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default DeployedApps;
+export default DeployedApps
--- components/UserProfile/Deployed_models.tsx
+++ components/UserProfile/Deployed_models.tsx
@@ -1,37 +1,60 @@
-import React from "react";
-import { Input } from "../ui/input";
-import { ListFilter, ChevronsLeft } from "lucide-react";
-import Deployed_Model_listing from "./Deployed_Model_listing";
+import React, { useState } from 'react'
+import { motion } from 'framer-motion'
+import { ChevronsLeft, ListFilter } from 'lucide-react'
+
+import { Input } from '../ui/input'
+import Deployed_Model_listing from './Deployed_Model_listing'
 
 interface Deployed_models_props {
-  show: boolean;
-  setShow: React.Dispatch<React.SetStateAction<boolean>>;
+  show: boolean
+  selectedChains?: string[]
+  selectedCategories?: string[]
+  setShow: React.Dispatch<React.SetStateAction<boolean>>
 }
 
-const Deployed_models = ({ show, setShow }: Deployed_models_props) => {
+const Deployed_models = ({
+  show,
+  setShow,
+  selectedCategories,
+  selectedChains,
+}: Deployed_models_props) => {
+  const [app, setApp] = useState('')
   return (
     <div className="h-full transition-all duration-700 ease-in-out">
-     
-      <div className="flex gap-4">
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{
+          duration: 0.4,
+        }}
+        className="flex gap-4"
+      >
         <div
-          className="border border-[#A3A3A3] cursor-pointer p-2 w-14 rounded-md flex justify-center items-center"
+          className="flex w-14 cursor-pointer items-center justify-center rounded-md border border-[#A3A3A3] p-2"
           onClick={() => setShow(!show)}
         >
           {show ? (
-            <ChevronsLeft className="text-[#A3A3A3] transition-transform duration-500 ease-in-out" />
+            <ChevronsLeft className="text-[#A3A3A3] transition-transform delay-150 duration-500 ease-in-out" />
           ) : (
-            <ListFilter className="text-[#A3A3A3] transition-transform duration-500 ease-in-out" />
+            <ListFilter className="text-[#A3A3A3] transition-transform delay-150 duration-500 ease-in-out" />
           )}
         </div>
         <Input
           placeholder="Search Apps"
-          className="w-full rounded-md focus:outline-none outline-none bg-transparent border border-[#A3A3A3] text-[#A3A3A3] px-2 py-3"
+          value={app}
+          onChange={(e) => setApp(e.target.value)}
+          className="w-full rounded-md border border-[#A3A3A3] bg-transparent px-2 py-3 text-[#A3A3A3] outline-none focus:border-[#A3A3A3] focus:outline-none focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
         />
-      </div>
+      </motion.div>
 
-      <Deployed_Model_listing show={show}/>
+      <Deployed_Model_listing
+        show={show}
+        selectedCategories={selectedCategories}
+        selectedChains={selectedChains}
+        app={app}
+      />
     </div>
-  );
-};
+  )
+}
 
-export default Deployed_models;
+export default Deployed_models
